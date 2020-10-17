a98999b893aa733a4e46bfa3faece677522d39fa
Adeynack <Adeynack@users.noreply.github.com>
2020-10-17 19:50:26 +0100

    updateUser

--- examples/ruby_on_rails_ruby_graphql/app/graphql/types/mutation_type.rb
+++ examples/ruby_on_rails_ruby_graphql/app/graphql/types/mutation_type.rb
@@ -2,6 +2,7 @@
 
 module Types
   class MutationType < Types::BaseObject
+    field :update_user, mutation: Mutations::UpdateUser
     field :create_user, mutation: Mutations::CreateUser
   end
 end

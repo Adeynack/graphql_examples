db11fcc809c3cf6eb52d3628858b38cb8eca8e13
Adeynack <Adeynack@users.noreply.github.com>
2023-10-30 22:07:33 +0100

    console feedback for every test

--- tests/jest-environment-fail-fast.ts
+++ tests/jest-environment-fail-fast.ts
@@ -13,9 +13,28 @@ class FailFastEnvironment extends TestEnvironment {
       event.test.mode = 'skip';
     }
 
+    this.visualFeedback(event);
+
     // eslint-disable-next-line @typescript-eslint/ban-ts-comment
     // @ts-ignore
     if (super.handleTestEvent) await super.handleTestEvent(event, state);
+  }
+
+  private visualFeedback(event: Circus.Event) {
+    switch (event.name) {
+      case 'hook_failure':
+        console.log(`[FAIL] Hook failure`);
+        break;
+      case 'test_fn_failure':
+        console.log(`[FAIL] ${event.test.name}`);
+        break;
+      case 'test_start':
+        if (this.failedTest) console.log(`[SKIP] ${event.test.name}`);
+        break;
+      case 'test_fn_success':
+        console.log(`[ OK ] ${event.test.name}`);
+        break;
+    }
   }
 }
 

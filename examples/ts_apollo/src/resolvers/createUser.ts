888f5420d9c54c50501eb73a955df21a2070e9ac
Adeynack <Adeynack@users.noreply.github.com>
2023-11-30 17:03:52 +0100

    basic create user

--- examples/ts_apollo/src/resolvers/createUser.ts
+++ examples/ts_apollo/src/resolvers/createUser.ts
@@ -1,12 +1,15 @@
 import { MutationResolvers } from '../__generated__/graphql';
+import { generatePasswordDigest } from '../models/user.js';
 
 const createUser: MutationResolvers['createUser'] = async (
   _parent,
-  { input: { clientMutationId } },
-  { db, authorize }
+  { input: { clientMutationId, email, name, password } },
+  { db, authorize, serverSalt }
 ) => {
   await authorize();
-  return { clientMutationId };
+  const passwordDigest = generatePasswordDigest(serverSalt, password);
+  const user = await db.user.create({ data: { email, name, passwordDigest } });
+  return { clientMutationId, user };
 };
 
 export default createUser;

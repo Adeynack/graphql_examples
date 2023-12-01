0ad0ade1915eabbd7426e3b22c274d21cdc1553c
Adeynack (David) <Adeynack@users.noreply.github.com>
2023-12-01 17:27:48 +0100

    Split into multiple dev containers (#5)
    * wip
    
    * made it work!

--- examples/ts_apollo/src/index.ts
+++ examples/ts_apollo/src/index.ts
@@ -1,37 +1,34 @@
-import { ApolloServer } from '@apollo/server';
-import { startStandaloneServer } from '@apollo/server/standalone';
-import { readFileSync } from 'fs';
+/* eslint-disable no-console */
 import { createContextFactory } from './context.js';
-import { Resolvers } from './__generated__/graphql.js';
 import { config } from 'dotenv';
-import query from './resolvers/query.js';
-import post from './resolvers/post.js';
-import user from './resolvers/users.js';
-import mutation from './resolvers/mutation.js';
-import { LoggingPlugin } from './loggingPlugin.js';
+import express from 'express';
+import { env } from 'process';
+import { expressMiddleware } from '@apollo/server/express4';
+import http from 'http';
+import { createApolloServer } from './apolloServer.js';
+import cors from 'cors';
+import { exec } from 'child_process';
+import { promisify } from 'util';
 
 config({ path: '.env' });
-const typeDefs = readFileSync('src/typeDefs.graphql').toString();
 
-const resolvers: Resolvers = {
-  Query: query,
-  Mutation: mutation,
-  Post: post,
-  User: user,
-};
-
-const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-  plugins: [new LoggingPlugin()],
-});
-
-const { url } = await startStandaloneServer(server, {
-  listen: { port: 4000 },
+const port = env.port || 30101;
+const app = express();
+const httpServer = http.createServer(app);
+const apolloServer = await createApolloServer(httpServer);
+const apolloServerExpressMiddleware = expressMiddleware(apolloServer, {
   context: createContextFactory(),
 });
 
-// eslint-disable-next-line no-console
-console.log(`🚀  Server ready at: ${url}`);
+app.use('/graphql', cors(), express.json(), apolloServerExpressMiddleware);
 
-// NEXT STEPS: https://www.apollographql.com/docs/apollo-server/data/errors
+app.post('/data_resets', async (_req, res) => {
+  console.log('Data reset triggered...');
+  await promisify(exec)('npx prisma db seed -- --environment development --truncateAllData');
+  console.log('Data reset completed.');
+  res.send('OK\n');
+});
+
+app.listen(port, () => {
+  console.log(`🚀  Server ready on port ${port}`);
+});

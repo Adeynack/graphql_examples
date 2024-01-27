471e0c01bbdfe60daa5217a08f0305e3ac8f5f22
Adeynack <Adeynack@users.noreply.github.com>
2024-01-27 15:13:02 +0100

    proper use of go generate

--- examples/go/graph/resolver.go
+++ examples/go/graph/resolver.go
@@ -1,5 +1,7 @@
 package graph
 
+//go:generate go run github.com/99designs/gqlgen generate
+
 import "github.com/adeynack/graphql_examples/examples/go/graph/model"
 
 // This file will not be regenerated automatically.

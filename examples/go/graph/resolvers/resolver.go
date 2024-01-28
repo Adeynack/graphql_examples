fbf210f37b9adf54f9de9a35cbb8aa257d386146
Adeynack <Adeynack@users.noreply.github.com>
2024-01-28 15:47:09 +0100

    dev setup

--- examples/go/graph/resolvers/resolver.go
+++ examples/go/graph/resolvers/resolver.go
@@ -1,12 +1,8 @@
 package resolvers
 
-import "github.com/adeynack/graphql_examples/examples/go/graph/model"
-
 // This file will not be regenerated automatically.
 //
 // It serves as dependency injection for your app, add any dependencies you require here.
 
 type Resolver struct {
-	users []*model.User
-	posts []*model.Post
 }

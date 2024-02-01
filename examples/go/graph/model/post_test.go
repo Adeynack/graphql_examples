04136244045e6e5ea9a0bde9aca9ece73f6be4bf
Adeynack <Adeynack@users.noreply.github.com>
2024-02-01 12:48:56 +0100

    refactor test

--- examples/go/graph/model/post_test.go
+++ examples/go/graph/model/post_test.go
@@ -25,39 +25,33 @@ func Test_Post_AuthorAssociation(t *testing.T) {
 		assert.NotEmpty(t, post.ID) // creating did assign an ID to the object
 		assert.Equal(t, anton.ID, post.AuthorID)
 
-		// Retrieving that post by ID should not preload the author when not explicitly requested.
-		var post1 *Post
-		tx = db.First(&post1, post.ID)
-		if !assert.NoError(t, tx.Error) {
-			return
-		}
-		assert.Equal(t, anton.ID, post1.AuthorID)
-		assert.Nil(t, post1.Author)
-
-		// Retrieving that post by ID preloading its author should fill both AuthorID and Author.
-		var post2 *Post
-		tx = db.Preload("Author").First(&post2, post.ID)
-		if !assert.NoError(t, tx.Error) {
-			return
-		}
-		assert.Equal(t, anton.ID, post2.AuthorID)
-		assert.NotNil(t, post2.Author)
-		assert.Equal(t, anton.ID, post2.Author.ID)
-		assert.Equal(t, anton, post2.Author)
+		_ = test_Post_DoNotPreloadAuthorByDefault(t, db, post, anton) &&
+			test_Post_PreloadAuthorWhenExplicit(t, db, post, anton)
 	})
 }
 
-func createAnton(db *gorm.DB) (user *User, err error) {
-	user = &User{
-		Email: "anton@example.com",
-		Name:  "Anton",
+func test_Post_DoNotPreloadAuthorByDefault(t *testing.T, db *gorm.DB, originalPost *Post, anton *User) bool {
+	// Retrieving that post by ID should not preload the author when not explicitly requested.
+	var post *Post
+	tx := db.First(&post, originalPost.ID)
+	if !assert.NoError(t, tx.Error) {
+		return false
 	}
-	user.SetPassword("password-for-anton", "salt")
-	err = db.Create(user).Error
-	if err != nil {
-		return
+	assert.Equal(t, anton.ID, post.AuthorID)
+	assert.Nil(t, post.Author)
+	return true
+}
+
+func test_Post_PreloadAuthorWhenExplicit(t *testing.T, db *gorm.DB, originalPost *Post, anton *User) bool {
+	// Retrieving that post by ID preloading its author should fill both AuthorID and Author.
+	var post *Post
+	tx := db.Preload("Author").First(&post, originalPost.ID)
+	if !assert.NoError(t, tx.Error) {
+		return false
 	}
-	// Reloading, since timestamps obtained on select are millisecond-truncated vs those set at create in-memory
-	err = db.First(&user, user.ID).Error
-	return
+	assert.Equal(t, anton.ID, post.AuthorID)
+	assert.NotNil(t, post.Author)
+	assert.Equal(t, anton.ID, post.Author.ID)
+	assert.Equal(t, anton, post.Author)
+	return true
 }
---
+++ examples/go/graph/model/user_test.go
@@ -0,0 +1,18 @@
+package model
+
+import "gorm.io/gorm"
+
+func createAnton(db *gorm.DB) (user *User, err error) {
+	user = &User{
+		Email: "anton@example.com",
+		Name:  "Anton",
+	}
+	user.SetPassword("password-for-anton", "salt")
+	err = db.Create(user).Error
+	if err != nil {
+		return
+	}
+	// Reloading, since timestamps obtained on select are millisecond-truncated vs those set at create in-memory
+	err = db.First(&user, user.ID).Error
+	return
+}

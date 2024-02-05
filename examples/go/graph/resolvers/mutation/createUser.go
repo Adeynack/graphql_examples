1fff146ba454ce775b0199d394ab4bce902a71a3
Adeynack <Adeynack@users.noreply.github.com>
2024-02-05 23:57:05 +0100

    trying to add sylvia@example.com again fails

--- examples/go/graph/resolvers/mutation/createUser.go
+++ examples/go/graph/resolvers/mutation/createUser.go
@@ -3,6 +3,7 @@ package mutation
 import (
 	"fmt"
 
+	"github.com/adeynack/graphql_examples/examples/go/graph"
 	"github.com/adeynack/graphql_examples/examples/go/graph/model"
 	"github.com/adeynack/graphql_examples/examples/go/graph/resolvers/gqlpolicy"
 	"github.com/adeynack/graphql_examples/examples/go/service"
@@ -13,6 +14,11 @@ func CreateUser(ctx service.ReqCtx, input model.CreateUserInput) (*model.CreateU
 	if err := gqlpolicy.Authenticated(ctx); err != nil {
 		return response, err
 	}
+	if err := checkForExistingUserPerEmail(ctx, input.Email); err != nil {
+		return response, err
+	}
+
+	// Creating new user
 	response.User = &model.User{
 		Email: input.Email,
 		Name:  input.Name,
@@ -24,4 +30,15 @@ func CreateUser(ctx service.ReqCtx, input model.CreateUserInput) (*model.CreateU
 		return response, fmt.Errorf("error creating new user: %v", err)
 	}
 	return response, nil
+}
+
+func checkForExistingUserPerEmail(ctx service.ReqCtx, email string) error {
+	var existingUserCount int64
+	if err := ctx.DB.Model(&model.User{}).Where("lower(users.email) = lower(?)", email).Count(&existingUserCount).Error; err != nil {
+		return fmt.Errorf("error checking for existing user per email: %v", err)
+	}
+	if existingUserCount > 0 {
+		return graph.UserFacingError("Email has already been taken")
+	}
+	return nil
 }
